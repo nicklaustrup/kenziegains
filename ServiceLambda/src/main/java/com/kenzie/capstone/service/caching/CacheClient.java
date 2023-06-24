e57f67faee58a92cd0ec26c154e0eddc72646e2e
Zachary C.B <zachary.caraballobobea@mykenzie.snhu.edu>
2023-06-24 15:38:27 -0400

    caching progress

---
+++ ServiceLambda/src/main/java/com/kenzie/capstone/service/caching/CacheClient.java
@@ -0,0 +1,29 @@
+package com.kenzie.capstone.service.caching;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.inject.Inject;
+
+public class CacheClient {
+        private final JedisPool jedisPool;
+        @Inject
+        public CacheClient(JedisPool jedisPool) {
+            this.jedisPool = jedisPool;
+        }
+        public void setValue(String key, int seconds, String val) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                jedis.setex(key, seconds, val);
+            }
+        }
+        public String getValue(String key) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                return jedis.get(key);
+            }
+        }
+        public void deleteValue(String key) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                jedis.del(key);
+            }
+        }
+}
