c559953cf4533468713907c4c158a7540fb2a97b
Nicholas Laustrup <124007393+nicklaustrup@users.noreply.github.com>
2025-03-22 14:19:22 -0700

    removed more mocks

--- Application/src/main/java/com/kenzie/appserver/config/LambdaServiceClientConfiguration.java
+++ Application/src/main/java/com/kenzie/appserver/config/LambdaServiceClientConfiguration.java
@@ -1,39 +1,39 @@
-// package com.kenzie.appserver.config;
-
-// import com.kenzie.capstone.service.client.LambdaServiceClient;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-
-// @Configuration
-// public class LambdaServiceClientConfiguration {
-
-//     @Bean
-//     public LambdaServiceClient referralServiceClient() {
-//         return new LambdaServiceClient();
-//     }
-// }
-
 package com.kenzie.appserver.config;
 
 import com.kenzie.capstone.service.client.LambdaServiceClient;
-import com.kenzie.appserver.service.mock.MockLambdaServiceClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class LambdaServiceClientConfiguration {
 
     @Bean
-    @Profile("!local")
-    public LambdaServiceClient lambdaServiceClient() {
+    public LambdaServiceClient referralServiceClient() {
         return new LambdaServiceClient();
     }
+}
 
-    @Bean
-    @Profile("local")
-    public LambdaServiceClient mockLambdaServiceClient() {
-        return new MockLambdaServiceClient();
-    }
+// package com.kenzie.appserver.config;
+
+// import com.kenzie.capstone.service.client.LambdaServiceClient;
+// import com.kenzie.appserver.service.mock.MockLambdaServiceClient;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Profile;
+
+// @Configuration
+// public class LambdaServiceClientConfiguration {
+
+//     @Bean
+//     @Profile("!local")
+//     public LambdaServiceClient lambdaServiceClient() {
+//         return new LambdaServiceClient();
+//     }
+
+//     @Bean
+//     @Profile("local")
+//     public LambdaServiceClient mockLambdaServiceClient() {
+//         return new MockLambdaServiceClient();
+//     }
 
 }
